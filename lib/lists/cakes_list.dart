ba35a9fe4b5eb90fe359a875f95fc7c3986f264f
JIAWEI68 <83011056+JIAWEI68@users.noreply.github.com>
2022-05-29 19:35:46 +0800

    added images and tried doing lists

---
+++ lib/lists/cakes_list.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:recipe_app/models/cakes.dart';
+
+class CakeList extends StatelessWidget {
+  List <Cake> cakesList = [
+    Cake(image: 'assets/christmascake.jpg')
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
